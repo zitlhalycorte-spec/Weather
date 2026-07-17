4f29bead461b5a4dd7c70c675634d5455edb4847
Itzamar <zitlhalycorte@gmail.com>
2026-07-17 11:59:38 -0700

    Updated seach functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+        let cityElement = document.querySelector("#city");
+    
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+
+}
+
+function searchCity(city) {
+    let apiKey = "4o0f0d00b99adfd3cfc2458ct6974d63";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Fontana");
