32520c49869f36a680176a533d6093dd4f7b707d
Itzamar <zitlhalycorte@gmail.com>
2026-07-24 08:41:52 -0700

    added 5 day weather forecast

--- src/index.js
+++ src/index.js
@@ -26,6 +26,68 @@ function refreshWeather(response) {
     displayWeather(response);
 }
 
+
+function formatForecastDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+}
+
+function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHtml = "";
+
+    let localIconMap = {
+        "clear-sky-day": "icons/sunny.gif",
+        "clear-sky-night": "icons/night.gif",
+        "few-clouds-day": "icons/cloudy.gif",
+        "few-clouds-night": "icons/cloudy night.gif",
+        "scattered-clouds-day": "icons/cloudy.gif",
+        "scattered-clouds-night": "icons/cloudy night.gif",
+        "broken-clouds-day": "icons/cloudy.gif",
+        "broken-clouds-night": "icons/cloudy night.gif",
+        "shower-rain-day": "icons/rainy.gif",
+        "shower-rain-night": "icons/rainy.gif",
+        "rain-day": "icons/rainy.gif",
+        "rain-night": "icons/rainy.gif",
+        "thunderstorm-day": "icons/thundering.gif",
+        "thunderstorm-night": "icons/thundering.gif",
+        "snow-day": "icons/snowing.gif",
+        "snow-night": "icons/snowing.gif",
+        "mist-day": "icons/rainy.gif",
+        "mist-night": "icons/rainy.gif"
+    };
+
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+
+            let apiIconCode = day.condition.icon;
+
+            let customIconPath = localIconMap[apiIconCode] || "icons/sunny.gif";
+
+            forecastHtml += `
+                <div class="weather-forecast-day">
+                    <div class="weather-forecast-date">${formatForecastDay(day.time)}</div>
+                    <img src="${customIconPath}" class="weather-forecast-icon" alt="${day.condition.description}"/>
+                    <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temp-max">
+                            ${Math.round(day.temperature.maximum)}°
+                        </span>
+                        <span class="weather-forecast-temp-min">
+                            ${Math.round(day.temperature.minimum)}°
+                        </span>
+                    </div>
+                </div>
+            `;
+        }
+    });
+
+    forecastElement.innerHTML = forecastHtml;
+}
+
+
+
+
 function getCityTime(timezoneName) {
   return new Intl.DateTimeFormat('en-US', {
     timeStyle: 'medium',
@@ -132,6 +194,9 @@ function searchCity(city) {
     let apiKey = "4o0f0d00b99adfd3cfc2458ct6974d63";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(refreshWeather);
+    
+    let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(forecastApiUrl).then(displayForecast);
 }
 
 
